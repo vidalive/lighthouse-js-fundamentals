665d0ebab11d0a20d0f4b09fb128ae362c875321
Vida Moayedi <vidalive@gmail.com>
2020-04-22 00:44:57 +0000

    add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -24,14 +24,36 @@
 
 /////////////////////////////////////////////////
 
-const temperature = 0;
+// const temperature = 0;
 
-if (temperature < 0) {
-  console.log("Make sure you pick out a scarf!");
-} else if (temperature < 15) {
-  console.log("Short sleeves won't cut it!");
-} else {
-  console.log("Short sleeves are fine.");
+// if (temperature < 0) {
+//   console.log("Make sure you pick out a scarf!");
+// } else if (temperature < 15) {
+//   console.log("Short sleeves won't cut it!");
+// } else {
+//   console.log("Short sleeves are fine.");
+// }
+
+// console.log("Now you're ready to go outside!");
+
+/////////////////////////////////////////////////
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  Console.log("You are eligible to vote.");
+}
+/////////////////////////////////////////////////
+
+if (tempetature < -40 || temperature > 40) {
+  Console.log("Maybe going outside isn't such a great idea…");
 }
 
-console.log("Now you're ready to go outside!");
+/////////////////////////////////////////////////
+
+if (!raining) {
+  Console.log("Leave your umbrealla at home!");
+
+}
+
